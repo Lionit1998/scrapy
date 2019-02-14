c5712e2bf02373a27dd1a0e526faa32deb26a2b4
Lionit1998 <Lionit1998@outlook.com>
2019-02-14 18:18:36 +0800

    0214evening

---
+++ spider4.py
@@ -0,0 +1,10 @@
+# -*- coding : UTF-8 -*-
+from urllib import request
+
+if __name__ == "__main__":
+    req = request.Request("http://fanyi.baidu.com")
+    response = request.urlopen(req)
+
+print("geturl打印信息 ：%s"%response.geturl())
+print("info打印信息 ：%s"%response.info()) 
+print("getcode打印信息 ：%s"%response.getcode())    
